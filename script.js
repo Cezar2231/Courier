7bd0d1471495588b036765d0626169637f6ffddd
Stoil Georgiev <117761845+Cezar2231@users.noreply.github.com>
2026-05-06 22:34:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -171,6 +171,22 @@ document.addEventListener('click', function (event) {
     }
 });
 
+function openLightbox(src) {
+  const lightbox = document.getElementById("lightbox");
+  const img = document.getElementById("lightbox-img");
+
+  img.src = src;
+  lightbox.classList.remove("hidden");
+  lightbox.classList.add("flex");
+}
+
+function closeLightbox() {
+  const lightbox = document.getElementById("lightbox");
+
+  lightbox.classList.add("hidden");
+  lightbox.classList.remove("flex");
+}
+
 
 
 
